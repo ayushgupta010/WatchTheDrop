49623e213789106a58ee7d603d83145c49dbdd5b
Ayush Gupta <ayushgupta83681@gmail.com>
2026-02-14 02:53:20 +0530

    feat: add Mongoose utility for connecting to MongoDB.

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -5,7 +5,7 @@ let isConnected = false;// Variable to track the connection status
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if (!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
+  if (!process.env.MONGODB_URI) throw new Error('MONGODB_URI is not defined');
 
   if (isConnected) return console.log('=> using existing database connection');
 
